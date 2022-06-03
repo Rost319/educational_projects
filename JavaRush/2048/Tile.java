4915165f270fa30c37410badf06eb9f1ee64fb54
Rost319 <75014905+Rost319@users.noreply.github.com>
2022-06-03 20:13:08 +0700

    Update Tile.java

--- JavaRush/2048/Tile.java
+++ JavaRush/2048/Tile.java
@@ -3,59 +3,52 @@ package com.javarush.task.task35.task3513;
 import java.awt.*;
 
 public class Tile {
-     int value;
+    int value = 0;
 
-    public Tile(int value) {
-        this.value = value;
+    Tile() {
     }
 
-    public Tile() {
-        this.value = 0;
+    Tile(int num) {
+        value = num;
     }
 
     public boolean isEmpty() {
-        if(value == 0){
-            return true;
-        } else return false;
+        return value == 0;
     }
 
-    public Color getFontColor() {
-        if(value < 16){
-            return new Color(0x776e65);
-        } else
-            return new Color(0xf9f6f2);
+    Color getFontColor() {
+        return value < 16 ? new Color(0x776e65) : new Color(0xf9f6f2);
     }
 
-    public Color getTileColor() {
-        Color color;
+    Color getTileColor() {
         switch (value) {
-            case 0: color = new Color(0xcdc1b4);
-            break;
-            case 2: color = new Color(0xeee4da);
-            break;
-            case 4: color = new Color(0xede0c8);
-            break;
-            case 8: color = new Color(0xf2b179);
-            break;
-            case 16: color = new Color(0xf59563);
-            break;
-            case 32: color = new Color(0xf67c5f);
-            break;
-            case 64: color = new Color(0xf65e3b);
-            break;
-            case 128: color = new Color(0xedcf72);
-            break;
-            case 256: color = new Color(0xedcc61);
-            break;
-            case 512: color = new Color(0xedc850);
-            break;
-            case 1024: color = new Color(0xedc53f);
-            break;
-            case 2048: color = new Color(0xedc22e);
-            break;
-            default: color = new Color(0xff0000);
+            case 0:
+                return new Color(0xcdc1b4);
+            case 2:
+                return new Color(0xeee4da);
+            case 4:
+                return new Color(0xede0c8);
+            case 8:
+                return new Color(0xf2b179);
+            case 16:
+                return new Color(0xf59563);
+            case 32:
+                return new Color(0xf67c5f);
+            case 64:
+                return new Color(0xf65e3b);
+            case 128:
+                return new Color(0xedcf72);
+            case 256:
+                return new Color(0xedcc61);
+            case 512:
+                return new Color(0xedc850);
+            case 1024:
+                return new Color(0xedc53f);
+            case 2048:
+                return new Color(0xedc22e);
 
+            default:
+                return new Color(0xff0000);
         }
-        return color;
     }
 }
